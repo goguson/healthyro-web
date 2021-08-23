fe9a23f545d6cee35f07401fb96390c06c93b063
Dominik Gogola <dominik.gogola@gmail.com>
2021-08-23 21:16:12 +0200

    - added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: process.env.TAILWIND_MODE ? 'jit' : 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
