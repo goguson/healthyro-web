0cb69510710a5a854ea7586b15e5407bb817a990
Dominik Gogola <dominik.gogola@gmail.com>
2021-09-28 19:10:04 +0200

    - wip format kompa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  mode: process.env.TAILWIND_MODE ? 'jit' : 'jit',
-  purge: [],
+  mode: 'jit',
+  purge: {
+    content: ['./src/**/*.{html,ts}'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkTheme: '#212121',
+        primary: '#93C5FD',
+        secondary: '#6EE7B7',
+        danger: '#CF6679',
+      },
+      opacity: {
+        se: '.05',
+        me: '.08',
+        be: '.12',
+        'text-high': '.87',
+        'text-medium': '.60',
+        'text-disabled': '.38',
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
